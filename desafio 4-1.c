dc00b680a19a50989892251d3b1609f417eb9f0a
thurtrajano <arthurtcorreia@gmail.com>
2025-03-20 15:28:32 -0300

    Add files via upload

---
+++ desafio 4-1.c
@@ -0,0 +1,114 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                       _              //
+//               _    _       _      _        _     _   _   _    _   _   _        _   _  _   _          //
+//           |  | |  |_| |\| |_| |\ |_|   |\ |_|   |_| |_| | |  |   |_| |_| |\/| |_| |  |_| | |   /|    //    
+//         |_|  |_|  |\  | | | | |/ | |   |/ | |   |   |\  |_|  |_| |\  | | |  | | | |_ | | |_|   _|_   //
+//                                                                                       /              //
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+*   Programa básico para controle da placa durante a Jornada da Programação 1
+*   Permite o controle das entradas e saídas digitais, entradas analógicas, display LCD e teclado. 
+*   Cada biblioteca pode ser consultada na respectiva pasta em componentes
+*   Existem algumas imagens e outros documentos na pasta Recursos
+*   O código principal pode ser escrito a partir da linha 86
+*/
+
+// Área de inclusão das bibliotecas
+//-----------------------------------------------------------------------------------------------------------------------
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+#include "HCF_IOTEC.h"   // Vai se tornar HCF_IOTEC
+#include "HCF_LCD.h" // Vai se tornar HCF_LCD
+#include "HCF_ADC.h"   // Vai se tornar HCF_ADC
+#include "HCF_MP.h"   // Vai se tornar HCF_MP
+// Incluir HCF_IOT HCF_BT HCF_DHT HCF_ULTRA HCF_RFID HCF_ZMPT HCF_ACS HCF_SERVO HCF_OLED HCF_CAM HCF_SD HCF_LORA
+
+
+// Área das macros
+//-----------------------------------------------------------------------------------------------------------------------
+
+#define IN(x) (entradas>>x)&1
+
+// Área de declaração de variáveis e protótipos de funções
+//-----------------------------------------------------------------------------------------------------------------------
+
+static const char *TAG = "Placa";
+static uint8_t entradas, saidas = 0; //variáveis de controle de entradas e saídas
+static char tecla = '-' ;
+char escrever[40];
+
+// Funções e ramos auxiliares
+//-----------------------------------------------------------------------------------------------------------------------
+
+
+// Programa Principal
+//-----------------------------------------------------------------------------------------------------------------------
+
+void app_main(void)
+{
+    /////////////////////////////////////////////////////////////////////////////////////   Programa principal
+    escrever[39] = '\0';
+
+    // a seguir, apenas informações de console, aquelas notas verdes no início da execução
+    ESP_LOGI(TAG, "Iniciando...");
+    ESP_LOGI(TAG, "Versão do IDF: %s", esp_get_idf_version());
+
+    /////////////////////////////////////////////////////////////////////////////////////   Inicializações de periféricos (manter assim)
+    
+    // inicializar os IOs e teclado da placa
+    iniciar_iotec();      
+    entradas = io_le_escreve(saidas); // Limpa as saídas e lê o estado das entradas
+
+    // inicializar o display LCD 
+    iniciar_lcd();
+    escreve_lcd(1,0,"    Jornada 1   ");
+    escreve_lcd(2,0," Programa Basico");
+    
+    // Inicializar o componente de leitura de entrada analógica
+    esp_err_t init_result = iniciar_adc_CHX(0);
+    if (init_result != ESP_OK) {
+        ESP_LOGE("MAIN", "Erro ao inicializar o componente ADC personalizado");
+    }
+
+    //delay inicial
+    vTaskDelay(1000 / portTICK_PERIOD_MS); 
+    limpar_lcd();
+
+    /////////////////////////////////////////////////////////////////////////////////////   Periféricos inicializados
+
+ 
+
+    /////////////////////////////////////////////////////////////////////////////////////   Início do ramo principal                    
+    while (1)                                                                                                                         
+    {                                                                                                                                 
+        //_______________________________________________________________________________________________________________________________________________________ //
+        //-  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  - -  -  -  -  -  -  -  -  -  -  Escreva seu código aqui!!! //
+        tecla = le_teclado();
+
+        escreve_lcd(1,0,"APERTE  = ");
+        if(tecla == '='){
+        saidas = 0b00000001;
+        entradas = io_le_escreve(saidas);
+        escreve_lcd(1,0,"ABERTO");
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        saidas = 0b00000000;
+        entradas = io_le_escreve(saidas);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+        }
+        
+        //-  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  - -  -  -  -  -  -  -  -  -  -  Escreva seu só até aqui!!! //
+        //________________________________________________________________________________________________________________________________________________________//
+        vTaskDelay(10 / portTICK_PERIOD_MS);    // delay mínimo obrigatório, se retirar, pode causar reset do ESP
+    }
+    
+    // caso erro no programa, desliga o módulo ADC
+    adc_limpar();
+
+    /////////////////////////////////////////////////////////////////////////////////////   Fim do ramo principal
+    
+}
